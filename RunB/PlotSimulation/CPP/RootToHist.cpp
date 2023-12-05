42b2afe0d2cf2964b8f71c5df73d93d004746588
jonasmuecke <mail@jonasmuecke.com>
2023-12-05 20:23:52 +0100

    im the dumbest person alive

--- RunB/PlotSimulation/CPP/RootToHist.cpp
+++ RunB/PlotSimulation/CPP/RootToHist.cpp
@@ -1,4 +1,4 @@
-int RootToHistSim()
+int RootToHist()
 {
 
   string ranges[13] = {"50to80","80to120","120to170","170to300","300to470","470to600","600to800","800to1000","1000to1400","1400to1800","1800to2400","2400to3200","3200"};
