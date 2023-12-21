786ebcf06a902a112e6f38895935fcc482cf8098
jonasmuecke <mail@jonasmuecke.com>
2023-12-21 15:48:29 +0100

    mdss

--- Simulation/CPP/ScaleSimulation.cpp
+++ Simulation/CPP/ScaleSimulation.cpp
@@ -62,7 +62,7 @@ int ScaleSimulation()
     tree.SetBranchAddress("genJetAK4_mass1",&genmass1Num);
 
 
-    TH1D Response20to40("Response20to40","Response20to40",20,20,40);
+    TH1D Response20to40("Response20to40","Response20to40",20,20,4000);
     Response20to40.Sumw2();
     TH1D Response40to60("Response40to60","Response40to60",20,40,60);
     Response40to60.Sumw2();
@@ -94,9 +94,9 @@ int ScaleSimulation()
         double Response = (pt1Num[0]-genpt1Num[0])/genpt1Num[0];
 
         //Fill hists with response for pt ranges of 20GeV from 0 to 1000
-        if(20 <= pt1Num[0] && pt1Num[0] < 40 && 20 <= genpt1Num[0] && genpt1Num[0] < 40){
+        //if(20 <= pt1Num[0] && pt1Num[0] < 40 && 20 <= genpt1Num[0] && genpt1Num[0] < 40){
           Response20to40.Fill(Response);
-        }
+        //}
         if(40 <= pt1Num[0] && pt1Num[0] < 60 && 40 <= genpt1Num[0] && genpt1Num[0] < 60){
           Response40to60.Fill(Response);
         }
