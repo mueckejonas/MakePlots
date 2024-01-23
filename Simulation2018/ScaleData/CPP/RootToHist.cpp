f462a220411b8d24fe8e38e2e82232f754b331ef
jonasmuecke <mail@jonasmuecke.com>
2024-01-23 14:03:08 +0100

    get 2018 simulation data

--- Simulation2018/ScaleData/CPP/RootToHist.cpp
+++ Simulation2018/ScaleData/CPP/RootToHist.cpp
@@ -13,8 +13,7 @@ int RootToHist()
     TChain tree("tree");   // name of the tree is the argument
     for (int j = 0; j < rangesNums[i]; ++j){
       string inputtree = "/pnfs/desy.de/cms/tier2/store/user/hinzmann/dijetangular/qcdUL18feb2023/dijetChiUL18_QCD_HT"+ranges[i]+"_RunII_106X_v2_"+to_string(j)+"_tree.root";
-      std::cout << inputtree << std::endl;
-      //tree.Add("/pnfs/desy.de/cms/tier2/store/user/hinzmann/dijetangular/qcdUL18feb2023/dijetChiUL18_QCD_HT"+ranges[i]+"_RunII_106X_v2_"+to_string(j)+"_tree.root");
+      tree.Add(inputtree);
     }
 
     //declare variables to Load from Root Tree
