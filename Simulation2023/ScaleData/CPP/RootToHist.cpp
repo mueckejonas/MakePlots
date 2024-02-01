f1250cd63c3a49488e3146adeed4dbc3696d63a7
jonasmuecke <mail@jonasmuecke.com>
2024-02-01 15:40:13 +0100

    calc raw and num values for simulation

--- Simulation2023/ScaleData/CPP/RootToHist.cpp
+++ Simulation2023/ScaleData/CPP/RootToHist.cpp
@@ -16,14 +16,14 @@ int RootToHist()
     //variables of Jet1
     float pt1Num[eventNum];
     float pt1rawNum[eventNum];
-    float pt1numNum[eventNum];
+    float pt1nomNum[eventNum];
     float y1Num[eventNum];
     int TightID1[eventNum];
 
 
     tree->SetBranchAddress("jetAK4_pt1",&pt1Num);
     tree->SetBranchAddress("jetAK4_pt1_raw",&pt1rawNum);
-    tree->SetBranchAddress("jetAK4_pt1_num",&pt1numNum);
+    tree->SetBranchAddress("jetAK4_pt1_nom",&pt1nomNum);
     tree->SetBranchAddress("jetAK4_y1",&y1Num);
     tree->SetBranchAddress("jetAK4_TightID1",&TightID1);
 
@@ -31,19 +31,19 @@ int RootToHist()
     pt1.Sumw2();
     TH1D pt1raw("Simpt1raw","ptraw for jet1 Sim",20,0,4000);
     pt1raw.Sumw2();
-    TH1D pt1num("Simpt1num","ptnum for jet1 Sim",20,0,4000);
-    pt1num.Sumw2();
+    TH1D pt1nom("Simpt1nom","ptnom for jet1 Sim",20,0,4000);
+    pt1nom.Sumw2();
 
     //variables of Jet2
     float pt2Num[eventNum];
     float pt2rawNum[eventNum];
-    float pt2numNum[eventNum];
+    float pt2nomNum[eventNum];
     float y2Num[eventNum];
     int TightID2[eventNum];
 
     tree->SetBranchAddress("jetAK4_pt2",&pt2Num);
     tree->SetBranchAddress("jetAK4_pt2_raw",&pt2rawNum);
-    tree->SetBranchAddress("jetAK4_pt2_num",&pt2numNum);
+    tree->SetBranchAddress("jetAK4_pt2_nom",&pt2nomNum);
     tree->SetBranchAddress("jetAK4_y2",&y2Num);
     tree->SetBranchAddress("jetAK4_TightID2",&TightID2);
 
@@ -51,19 +51,19 @@ int RootToHist()
     pt2.Sumw2();
     TH1D pt2raw("Simpt2raw","ptraw for jet1 Sim",20,0,4000);
     pt2raw.Sumw2();
-    TH1D pt2num("Simpt2num","ptnum for jet1 Sim",20,0,4000);
-    pt2num.Sumw2();
+    TH1D pt2nom("Simpt2nom","ptnom for jet1 Sim",20,0,4000);
+    pt2nom.Sumw2();
 
     //variables of Jet3
     float pt3Num[eventNum];
     float pt3rawNum[eventNum];
-    float pt3numNum[eventNum];
+    float pt3nomNum[eventNum];
     float y3Num[eventNum];
     int TightID3[eventNum];
 
     tree->SetBranchAddress("jetAK4_pt3",&pt3Num);
     tree->SetBranchAddress("jetAK4_pt3_raw",&pt3rawNum);
-    tree->SetBranchAddress("jetAK4_pt3_num",&pt3numNum);
+    tree->SetBranchAddress("jetAK4_pt3_nom",&pt3nomNum);
     tree->SetBranchAddress("jetAK4_y3",&y3Num);
     tree->SetBranchAddress("jetAK4_TightID3",&TightID3);
 
@@ -71,22 +71,22 @@ int RootToHist()
     pt3.Sumw2();
     TH1D pt3raw("Simpt3raw","ptraw for jet1 Sim",20,0,4000);
     pt3raw.Sumw2();
-    TH1D pt3num("Simpt3num","ptnum for jet1 Sim",20,0,4000);
-    pt3num.Sumw2();
+    TH1D pt3nom("Simpt3nom","ptnom for jet1 Sim",20,0,4000);
+    pt3nom.Sumw2();
 
     float mjjNum[eventNum];
-    float mjjnumNum[eventNum];
+    float mjjnomNum[eventNum];
     float mjjrawNum[eventNum];
 
     tree->SetBranchAddress("mjj",&mjjNum);
-    tree->SetBranchAddress("mjj_num",&mjjnumNum);
+    tree->SetBranchAddress("mjj_nom",&mjjnomNum);
     tree->SetBranchAddress("mjj_raw",&mjjrawNum);
 
     //Create quantities calculated from variables
     TH1D MjjHist("Simmjj","Mjj [Gev]",20,0,10000);
     MjjHist.Sumw2();
-    TH1D MjjHistNum("Simmjjnum","Mjj [Gev]",20,0,10000);
-    MjjHistNum.Sumw2();
+    TH1D MjjHistNom("Simmjjnom","Mjj [Gev]",20,0,10000);
+    MjjHistNom.Sumw2();
     TH1D MjjHistRaw("Simmjjraw","Mjj [Gev]",20,0,10000);
     MjjHistRaw.Sumw2();
 
@@ -124,26 +124,26 @@ int RootToHist()
           MjjHist.Fill(mjjNum[0]);
         }
       }
-      if (mjjnumNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
+      if (mjjnomNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
       {
         //Fill SimJet1
         if(TightID1[0] == 1){
-          pt1num.Fill(pt1numNum[0]);
+          pt1nom.Fill(pt1nomNum[0]);
         }
 
         //Fill SimJet2
         if(TightID2[0] == 1){
-          pt2num.Fill(pt2numNum[0]);
+          pt2nom.Fill(pt2nomNum[0]);
         }
 
         //Fill SimJet3
         if(TightID3[0] == 1){
-          pt3num.Fill(pt3numNum[0]);
+          pt3nom.Fill(pt3nomNum[0]);
         }
 
         if(TightID1[0] == 1 && TightID2[0] == 1){
           //fill SimMjj
-          MjjHistNum.Fill(mjjnumNum[0]);
+          MjjHistNom.Fill(mjjnomNum[0]);
         }
       }
       if (mjjrawNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
@@ -172,32 +172,32 @@ int RootToHist()
 
     //Neccesary so files dont get lost
     MjjHist.SetDirectory(0);
-    MjjHistNum.SetDirectory(0);
+    MjjHistNom.SetDirectory(0);
     MjjHistRaw.SetDirectory(0);
     pt3.SetDirectory(0);
     pt3raw.SetDirectory(0);
-    pt3num.SetDirectory(0);
+    pt3nom.SetDirectory(0);
     pt2.SetDirectory(0);
     pt2raw.SetDirectory(0);
-    pt2num.SetDirectory(0);
+    pt2nom.SetDirectory(0);
     pt1.SetDirectory(0);
     pt1raw.SetDirectory(0);
-    pt1num.SetDirectory(0);
+    pt1nom.SetDirectory(0);
 
 
     TFile* outHistFile = TFile::Open(outName.c_str(),"RECREATE");
     MjjHist.Write();
-    MjjHistNum.Write();
+    MjjHistNom.Write();
     MjjHistRaw.Write();
     pt3.Write();
     pt3raw.Write();
-    pt3num.Write();
+    pt3nom.Write();
     pt2.Write();
     pt2raw.Write();
-    pt2num.Write();
+    pt2nom.Write();
     pt1.Write();
     pt1raw.Write();
-    pt1num.Write();
+    pt1nom.Write();
     outHistFile->Close();
   }
   return 0;
