0db5f07f534cf9f32766dd7f7d6d30e0de01e63b
jonasmuecke <mail@jonasmuecke.com>
2024-02-02 10:50:30 +0100

    plot cpp for jan15 asymm

--- Simulation2023/PlotDijetAsymmetry/CPP/RootToHist.cpp
+++ Simulation2023/PlotDijetAsymmetry/CPP/RootToHist.cpp
@@ -5,8 +5,8 @@ int RootToHist()
 
   for (int i = 0; i < 13; i++)
   {
-    string rootFile = "/nfs/dust/cms/user/hinzmann/run2023/QCD_PT-"+ranges[i]+"_TuneCP5_13p6TeV_pythia8_Run3Summer23NanoAODv12-130X_mcRun3_2023_realistic_v14-v2_NANOAODSIM.root";
-    string outName = "/nfs/dust/cms/user/mueckejo/RootS/"+ranges[i]+"_PlotDijetAsymmetry_Run32023_MC.root";
+    string rootFile = "/nfs/dust/cms/user/hinzmann/run2023/QCD_PT-"+ranges[i]+"_TuneCP5_13p6TeV_pythia8_Run3Summer23NanoAODv12-130X_mcRun3_2023_realistic_v14-v2_NANOAODSIM-15Jan2023.root";
+    string outName = "/nfs/dust/cms/user/mueckejo/RootS2023/"+ranges[i]+"_PlotDijetAsymmetry_Run32023-15Jan2023_MC.root";
 
     TFile* inFile = TFile::Open(rootFile.c_str(),"READ");
     TTree* tree = (TTree*)inFile->Get("Events");
@@ -15,47 +15,50 @@ int RootToHist()
     const unsigned int eventNum = 1;
     //variables of Jet1
     float pt1Num[eventNum];
+    float pt1rawNum[eventNum];
+    float pt1nomNum[eventNum];
     float y1Num[eventNum];
-    float eta1Num[eventNum];
-    float phi1Num[eventNum];
-    float mass1Num[eventNum];
-    bool TightID1[eventNum];
+    int TightID1[eventNum];
+
 
     tree->SetBranchAddress("jetAK4_pt1",&pt1Num);
+    tree->SetBranchAddress("jetAK4_pt1_raw",&pt1rawNum);
+    tree->SetBranchAddress("jetAK4_pt1_nom",&pt1nomNum);
     tree->SetBranchAddress("jetAK4_y1",&y1Num);
-    tree->SetBranchAddress("jetAK4_eta1",&eta1Num);
-    tree->SetBranchAddress("jetAK4_phi1",&phi1Num);
-    tree->SetBranchAddress("jetAK4_mass1",&mass1Num);
     tree->SetBranchAddress("jetAK4_TightID1",&TightID1);
 
     //variables of Jet2
     float pt2Num[eventNum];
+    float pt2rawNum[eventNum];
+    float pt2nomNum[eventNum];
     float y2Num[eventNum];
-    float eta2Num[eventNum];
-    float phi2Num[eventNum];
-    float mass2Num[eventNum];
-    bool TightID2[eventNum];
+    int TightID2[eventNum];
 
     tree->SetBranchAddress("jetAK4_pt2",&pt2Num);
+    tree->SetBranchAddress("jetAK4_pt2_raw",&pt2rawNum);
+    tree->SetBranchAddress("jetAK4_pt2_nom",&pt2nomNum);
     tree->SetBranchAddress("jetAK4_y2",&y2Num);
-    tree->SetBranchAddress("jetAK4_eta2",&eta2Num);
-    tree->SetBranchAddress("jetAK4_phi2",&phi2Num);
-    tree->SetBranchAddress("jetAK4_mass2",&mass2Num);
     tree->SetBranchAddress("jetAK4_TightID2",&TightID2);
+
+    float mjjNum[eventNum];
+    float mjjnomNum[eventNum];
+    float mjjrawNum[eventNum];
+
+    tree->SetBranchAddress("mjj",&mjjNum);
+    tree->SetBranchAddress("mjj_nom",&mjjnomNum);
+    tree->SetBranchAddress("mjj_raw",&mjjrawNum);
 
     double pi = 3.14159265359;
     double radtodeg = 180.0/pi;
 
     TH1D PtAsymmetry("PtAsymmetry","PtAsymmetry",50,0,1);
     PtAsymmetry.Sumw2();
-    TH1D PhiDifference("PhiDifference","PhiDifference",50,0,180);
-    PhiDifference.Sumw2();
-    TH1D EtaDifference("EtaDifference","EtaDifference",50,-6,6);
-    EtaDifference.Sumw2();
-    TH1D YDifference("YDifference","YDifference",50,-6,6);
-    YDifference.Sumw2();
-    TH1D ThetaDifference("ThetaDifference","ThetaDifference",50,0,180);
-    ThetaDifference.Sumw2();
+
+    TH1D PtAsymmetryRaw("PtAsymmetryRaw","PtAsymmetryRaw",50,0,1);
+    PtAsymmetryRaw.Sumw2();
+
+    TH1D PtAsymmetryNom("PtAsymmetryNom","PtAsymmetryNom",50,0,1);
+    PtAsymmetryNom.Sumw2();
 
     std::cout << tree->GetEntries() << std::endl;
      float numberEntries = tree->GetEntries();
@@ -69,58 +72,41 @@ int RootToHist()
         std::cout << to_string((entry/numberEntries)*100) << "% finished" << std::endl;
       }
 
-      //Calculate Mjj
-    TLorentzVector Lorentz0, Lorentz1;
-    Lorentz0.SetPtEtaPhiM(pt1Num[0],eta1Num[0],phi1Num[0],mass1Num[0]);
-    Lorentz1.SetPtEtaPhiM(pt2Num[0],eta2Num[0],phi2Num[0],mass2Num[0]);
-    TLorentzVector MjjSum = Lorentz0 + Lorentz1;
-    double MjjValue = MjjSum.M();
-
     //Calculate chi
     double ChiValue = exp(abs(y1Num[0]-y2Num[0]));
 
     //Calculate yboost
     double YBoostValue = (y1Num[0]+y2Num[0])/2;
 
-    if (MjjValue > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11 && TightID1[0] == 1 && TightID2[0] == 1)
+    if (mjjNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
     {
-
-      if (abs(phi1Num[0]) > pi/2 && abs(phi2Num[0]) > pi/2) {
-      PhiDifference.Fill((abs(phi1Num[0])+abs(phi2Num[0])-pi)*radtodeg);
-      } else {
-        PhiDifference.Fill((abs(phi1Num[0])+abs(phi2Num[0]))*radtodeg);
+      if(TightID1[0] == 1 && TightID2[0] == 1){
+        PtAsymmetry.Fill((pt1Num[0]-pt2Num[0])/(pt1Num[0]+pt2Num[0]));
       }
-
-      double theta1 = 2*atan(exp(-eta1Num[0]));
-      double theta2 = 2*atan(exp(-eta2Num[0]));
-
-      if (theta1 > pi/2 && theta2 > pi/2) {
-        ThetaDifference.Fill((theta1+theta2-pi)*radtodeg);
-      } else {
-        ThetaDifference.Fill((theta1+theta2)*radtodeg);
+    }
+    if (mjjrawNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
+    {
+      if(TightID1[0] == 1 && TightID2[0] == 1){
+        PtAsymmetryRaw.Fill((pt1rawNum[0]-pt2rawNum[0])/(pt1rawNum[0]+pt2rawNum[0]));
       }
-
-
-      PtAsymmetry.Fill((pt1Num[0]-pt2Num[0])/(pt1Num[0]+pt2Num[0]));
-      EtaDifference.Fill(eta1Num[0]-eta2Num[0]);
-      YDifference.Fill(y1Num[0]-y2Num[0]);
-
+    }
+    if (mjjnomNum[0] > 2500 && ChiValue < 16 && abs(YBoostValue) < 1.11)
+    {
+      if(TightID1[0] == 1 && TightID2[0] == 1){
+        PtAsymmetryNom.Fill((pt1nomNum[0]-pt2nomNum[0])/(pt1nomNum[0]+pt2nomNum[0]));
+      }
     }
     }
 
     //Neccesary so files dont get lost
     PtAsymmetry.SetDirectory(0);
-    PhiDifference.SetDirectory(0);
-    EtaDifference.SetDirectory(0);
-    YDifference.SetDirectory(0);
-    ThetaDifference.SetDirectory(0);
+    PtAsymmetryRaw.SetDirectory(0);
+    PtAsymmetryNom.SetDirectory(0);
 
     TFile* outHistFile = TFile::Open(outName.c_str(),"RECREATE");
     PtAsymmetry.Write();
-    PhiDifference.Write();
-    EtaDifference.Write();
-    YDifference.Write();
-    ThetaDifference.Write();
+    PtAsymmetryRaw.Write();
+    PtAsymmetryNom.Write();
     outHistFile->Close();
   }
   return 0;
